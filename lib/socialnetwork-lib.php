59361e29e76a5226b94867697cbd0ea093f11862
keilaticona <keydy_26_12@hotmail.com>
2023-01-05 20:40:38 -0500

    iconos

--- lib/socialnetwork-lib.php
+++ lib/socialnetwork-lib.php
@@ -73,13 +73,13 @@ function Headerb ()
           <!-- Cuenta de usuario: el estilo se puede encontrar en dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="avatars/8.jpg" class="user-image" alt="User Image">
+              <img src="avatars/9.jpg" class="user-image" alt="User Image">
               <span class="hidden-xs"><?php echo ucwords($_SESSION['usuario']); ?></span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="avatars/8.jpg" class="img-circle" alt="User Image">
+                <img src="avatars/9.jpg" class="img-circle" alt="User Image">
 
                 <p>
                   <?php echo ucwords($_SESSION['usuario']); ?>
@@ -136,7 +136,7 @@ function Side ()
       <!-- Panel de usuario de la barra lateral -->
       <div class="user-panel">
         <div class="pull-left">
-          <img src="avatars/8.jpg" width="50" class="img-circle" alt="User Image">
+          <img src="avatars/9.jpg" width="50" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p><?php echo ucwords($_SESSION['usuario']); ?></p>
